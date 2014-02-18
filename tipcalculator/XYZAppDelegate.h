7fcad76d69efd335a17455a67d6ea4d1faab6a7d
Tushar Rawal <tushar.rawal@gmail.com>
2014-02-18 12:25:44 -0800

    Initial Commit

---
+++ tipcalculator/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  tipcalculator
+//
+//  Created by Tushar Rawal on 2/18/14.
+//  Copyright (c) 2014 Tushar Rawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Tushar Rawal on 2/18/14.
+//  Copyright (c) 2014 Tushar Rawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
